3e18ff5c8011d50bbf0e4486d81fb8a02c07602a
Alex Tomlins <alex@tomlins.org.uk>
2015-09-03 15:52:34 +0100

    Add various logging messages to test endpoint
    Using different approaches.  This will allow us to test the logging
    implementation.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,11 @@
 class RootController < ApplicationController
   def index
+    Rails.logger.info "Message via Rails.logger.info"
+    Rails.logger.warn "Message via Rails.logger.warn"
+    puts "Message via puts"
+    $stdout.puts "Message via $stdout.puts"
+    $stderr.puts "Message via $stderr.puts"
+    STDOUT.puts "Message via STDOUT.puts"
+    STDERR.puts "Message via STDERR.puts"
   end
 end
